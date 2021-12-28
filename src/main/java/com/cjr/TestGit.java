61d4cecfbbe48d31130a80fc56fe618d16bf0497
cjr070111 <1678311370@qq.com>
2021-12-28 11:19:23 +0800

    crj:第二个版本信息

--- src/main/java/com/cjr/TestGit.java
+++ src/main/java/com/cjr/TestGit.java
@@ -3,5 +3,10 @@ package com.cjr;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("第一个githup程序");
+        run();
+    }
+
+    public static void run(){
+        System.out.println("完成登录操作");
     }
 }
